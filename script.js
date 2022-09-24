a0262f1e71d0437044b2f94554dcacd755e62396
kosei <harumi.kosei1106@gmail.com>
2022-09-24 21:04:30 +0900

    nineth-commit

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ function previewImage(obj)
 	var fileReader = new FileReader();
 	fileReader.onload = (function() {
 		document.getElementById('preview').src = fileReader.result;
-    alert("ok");
 	});
 	fileReader.readAsDataURL(obj.files[0]);
 }
+
+var elem = document.getElementById('range');
+var target = document.getElementById('value');
+var rangeValue = function (elem, target){
+  return function(evt){
+    target.innerHTML = elem.value;
+  }
+}
+elem.addEventListener('input', rangeValue(elem, target));
