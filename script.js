98d4b8a863797b2b97b87d91eb474b5a1b4c59b7
kosei <harumi.kosei1106@gmail.com>
2022-09-24 20:41:04 +0900

    eighth-commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function previewImage(obj)
 	var fileReader = new FileReader();
 	fileReader.onload = (function() {
 		document.getElementById('preview').src = fileReader.result;
+    alert("ok");
 	});
 	fileReader.readAsDataURL(obj.files[0]);
 }
